9abb5783c8c48bc5c39d8ddcf9b7d332865f93bd
Chelik12345 <139449605+Chelik12345@users.noreply.github.com>
2024-08-13 19:45:07 +0500

    Delete src/main/java/searchengine/model/IndexRepository.java

--- src/main/java/searchengine/model/IndexRepository.java
+++
@@ -1,8 +0,0 @@
-package searchengine.model;
-import searchengine.model.Index;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IndexRepository extends JpaRepository<Index, Long> {
-}
