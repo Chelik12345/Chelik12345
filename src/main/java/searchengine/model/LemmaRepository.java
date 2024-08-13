15b2923b0803800ea26d66e401ef9c0fbf550031
Chelik12345 <139449605+Chelik12345@users.noreply.github.com>
2024-08-13 19:39:57 +0500

    Delete src/main/java/searchengine/model/LemmaRepository.java

--- src/main/java/searchengine/model/LemmaRepository.java
+++
@@ -1,9 +0,0 @@
-package searchengine.model;
-import searchengine.model.Lemma;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LemmaRepository extends JpaRepository<Lemma, Long> {
-    Lemma findByLemma(String lemma);
-}
